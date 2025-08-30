d0103eff650f84272d6d3eed7ed1dd6e262a4c91
Mikhail Borisov <mikhail.boriso2014@gmail.com>
2025-08-31 01:01:23 +0500

    ADD wave encoder decoder

---
+++ text_main.py
@@ -0,0 +1,128 @@
+from typing import Iterable
+import torch
+
+from train import train
+
+
+def load_data() -> str:
+    with open("data/tinyshakespeare.txt", "r", encoding="utf-8") as f:
+        result = f.read()
+    return result
+
+
+def get_chars(text: str) -> str:
+    result = "".join(sorted(set(text)))
+    return result
+
+
+class Encoder:
+    __slots__ = ("stoi", "vocab_size")
+
+    def __init__(self, chars: str) -> None:
+        stoi = {ch: i for i, ch in enumerate(chars)}
+        self.stoi = stoi
+        self.vocab_size = len(stoi.keys())
+
+    def encode(self, text: str) -> tuple[int, ...]:
+        stoi = self.stoi
+        result = tuple(stoi[ch] for ch in text)
+        return result
+
+
+class Decoder:
+    __slots__ = "itos"
+
+    def __init__(self, chars: str) -> None:
+        self.itos = {i: ch for i, ch in enumerate(chars)}
+
+    def decode(self, tokens: Iterable[int]) -> str:
+        itos = self.itos
+        result = "".join(itos[ch] for ch in tokens)
+        return result
+
+
+# INIT: val_loss=1.82 train_time=188
+# ADD torch.compile: val_loss=1.83 train_time=49.67
+# UPD eval_interval = 5_000: val_loss=1.82 train_time=40.67
+# ADD flash-attention: val_loss=1.8 train_time=26.67
+# UPD bias=False in lm_head: val_loss=1.82 train_time=28.67
+# ADD RMSNorm: val_loss=1.82 train_time=28.0
+# ADD RoPE: val_loss=1.76 train_time=28.67
+# ADD SwiGLU: val_loss=1.74 train_time=29.67
+# ADD AdamWScheduleFree: val_loss=1.68 train_time=28.0
+
+
+def main() -> None:
+    n_embd = 64
+    block_size = 32
+    n_head = 4
+    n_layer = 4
+    learning_rate = 1e-2
+    betas = (0.9, 0.95)
+    weight_decay = 0.1
+    max_iters = 5_000
+    eval_interval = 5_000
+    eval_iters = 200
+    batch_size = 16
+    rmsnorm_eps = 1e-5
+    rope_theta = 10000
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    text = load_data()
+    chars = get_chars(text)
+    encoder = Encoder(chars)
+    decoder = Decoder(chars)
+    encoded_text = encoder.encode(text)
+    assert decoder.decode(encoded_text) == text
+    n = 3
+    val_loss_array = torch.zeros((n,))
+    train_time_array = torch.zeros((n,))
+    model, val_loss, train_time = train(
+        embedding=encoded_text,
+        vocab_size=encoder.vocab_size,
+        device=device,
+        n_embd=n_embd,
+        block_size=block_size,
+        n_head=n_head,
+        n_layer=n_layer,
+        learning_rate=learning_rate,
+        betas=betas,
+        weight_decay=weight_decay,
+        max_iters=max_iters,
+        eval_interval=eval_interval,
+        eval_iters=eval_iters,
+        batch_size=batch_size,
+        rmsnorm_eps=rmsnorm_eps,
+        rope_theta=rope_theta,
+    )
+    val_loss_array[0] = val_loss
+    train_time_array[0] = train_time
+    for i in range(1, n):
+        _, val_loss, train_time = train(
+            embedding=encoded_text,
+            vocab_size=encoder.vocab_size,
+            device=device,
+            n_embd=n_embd,
+            block_size=block_size,
+            n_head=n_head,
+            n_layer=n_layer,
+            learning_rate=learning_rate,
+            betas=betas,
+            weight_decay=weight_decay,
+            max_iters=max_iters,
+            eval_interval=eval_interval,
+            eval_iters=eval_iters,
+            batch_size=batch_size,
+            rmsnorm_eps=rmsnorm_eps,
+            rope_theta=rope_theta,
+        )
+        val_loss_array[i] = val_loss
+        train_time_array[i] = train_time
+    val_loss = float(torch.mean(val_loss_array))
+    train_time = float(torch.mean(train_time_array))
+    print(f"val_loss={round(val_loss, 2)} train_time={round(train_time, 2)}")
+    context = torch.zeros((1, 1), dtype=torch.int64, device=device)
+    print(decoder.decode(model.generate(context, max_new_tokens=2000)[0].tolist()))
+
+
+if __name__ == "__main__":
+    main()
---
+++ wave_main.py
@@ -0,0 +1,122 @@
+import torch
+
+from train import train
+
+
+def encode(
+    inp: torch.Tensor, vocab_size: int
+) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+    diff = torch.diff(inp, dim=0)
+    max_diff, _ = torch.max(torch.abs(diff), dim=0)
+    scale = vocab_size / max_diff / 2
+    result = (diff * scale).to(torch.int64) + vocab_size // 2
+    return inp[:1], scale, result
+
+
+def decode(
+    start: torch.Tensor, scale: torch.Tensor, inp: torch.Tensor, vocab_size: int
+) -> torch.Tensor:
+    diff = (inp - vocab_size // 2) / scale
+    result = torch.concat((start, diff), dim=0).cumsum(dim=0)
+    return result
+
+
+def mae(x: torch.Tensor, y: torch.Tensor) -> float:
+    result = torch.mean(torch.abs(x - y), dim=0)
+    return float(torch.mean(result))
+
+
+# ADD wave encoder decoder: val_loss=0.18 train_time=27.0 MAE=0.0061
+
+
+def main() -> None:
+    n_embd = 64
+    block_size = 32
+    n_head = 4
+    n_layer = 4
+    learning_rate = 1e-2
+    betas = (0.9, 0.95)
+    weight_decay = 0.1
+    max_iters = 5_000
+    eval_interval = 5_000
+    eval_iters = 200
+    batch_size = 16
+    rmsnorm_eps = 1e-5
+    rope_theta = 10000
+    vocab_size = 256
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    idx = torch.arange(1_000_000)
+    inp = torch.stack(
+        (
+            torch.sin(idx),
+            torch.cos(idx),
+        ),
+        dim=1,
+    )
+    start, scale, embedding = encode(inp, vocab_size)
+    torch.testing.assert_close(
+        decode(start, scale, embedding, vocab_size), inp, rtol=1, atol=0.5
+    )
+    n = 3
+    val_loss_array = torch.zeros((n,))
+    train_time_array = torch.zeros((n,))
+    _embedding = tuple(embedding.reshape(-1).tolist())
+    model, val_loss, train_time = train(
+        embedding=_embedding,
+        vocab_size=vocab_size,
+        device=device,
+        n_embd=n_embd,
+        block_size=block_size,
+        n_head=n_head,
+        n_layer=n_layer,
+        learning_rate=learning_rate,
+        betas=betas,
+        weight_decay=weight_decay,
+        max_iters=max_iters,
+        eval_interval=eval_interval,
+        eval_iters=eval_iters,
+        batch_size=batch_size,
+        rmsnorm_eps=rmsnorm_eps,
+        rope_theta=rope_theta,
+    )
+    val_loss_array[0] = val_loss
+    train_time_array[0] = train_time
+    for i in range(1, n):
+        _, val_loss, train_time = train(
+            embedding=tuple(embedding.reshape(-1).tolist()),
+            vocab_size=vocab_size,
+            device=device,
+            n_embd=n_embd,
+            block_size=block_size,
+            n_head=n_head,
+            n_layer=n_layer,
+            learning_rate=learning_rate,
+            betas=betas,
+            weight_decay=weight_decay,
+            max_iters=max_iters,
+            eval_interval=eval_interval,
+            eval_iters=eval_iters,
+            batch_size=batch_size,
+            rmsnorm_eps=rmsnorm_eps,
+            rope_theta=rope_theta,
+        )
+        val_loss_array[i] = val_loss
+        train_time_array[i] = train_time
+    val_loss = float(torch.mean(val_loss_array))
+    train_time = float(torch.mean(train_time_array))
+    context = torch.tensor((_embedding[:8],), dtype=torch.int64, device=device)
+    decoded = decode(
+        start,
+        scale,
+        model.generate(context, max_new_tokens=22)[0]
+        .reshape(-1, embedding.shape[1])
+        .to(device="cpu"),
+        vocab_size=vocab_size,
+    )
+    print(
+        f"val_loss={round(val_loss, 2)} train_time={round(train_time, 2)} MAE={round(mae(inp[:16], decoded), 4)}"
+    )
+
+
+if __name__ == "__main__":
+    main()
